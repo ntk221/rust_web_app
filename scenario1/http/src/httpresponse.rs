fe1b67c6c3edada8fa05f297923b3f31697509fa
ntk221 <92190938+ntk221@users.noreply.github.com>
2023-06-23 10:57:06 +0000

    ready HttpResponse struct

--- scenario1/http/src/httpresponse.rs
+++ scenario1/http/src/httpresponse.rs
@@ -1,1 +1,13 @@
+use std::collections::HashMap;
+use std::io::{Read, Write};
 
+#[derive(Debug, PartialEq, Clone)]
+pub struct HttpResponse<'a> {
+    version: &'a str,
+    status_code: &'a str,
+    status_text: &'a str,
+    headers: Option<HashMap<&'a str, &'a str>>,
+    body: Option<String>,
+}
+
+// Note the use of lifetime annotation 'a for all the member fields that are of reference types.
--- scenario1/http/src/lib.rs
+++ scenario1/http/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod httprequest;
+pub mod httpresponse;
 
 // This tells the compiler that we are creating a new publicly-accessible module called httprequest in the http library.
